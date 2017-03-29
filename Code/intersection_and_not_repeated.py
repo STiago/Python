07f593ec22905ffafeaf6c4fbae492303b1c96b1
STiago <victoriasantiago@correo.ugr.es>
2017-03-29 15:32:13 +0200

    Intersection

---
+++ Code/intersection_and_not_repeated.py
@@ -0,0 +1,16 @@
+a = [1,2,3,4,5]
+b = [1,5,6,7]
+
+notrepeated = []
+intersection = []
+
+c = a+b
+
+for i in c:
+    if i not in notrepeated:
+        notrepeated.append(i)
+    else:
+        if i not in intersection:
+            intersection.append(i)
+
+print intersection
---
+++ Code/simple_intersection.py
@@ -0,0 +1,10 @@
+a = [1,2,3,4,5]
+b = [1,5,6,7]
+
+solution = []
+
+for i in a:
+    if i in b:
+        solution.append(i)
+
+print solution
