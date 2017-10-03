b5edc03b650dbb60f49e11ee78ed2ffc4ce37731
STiago <victoriasantiago@correo.ugr.es>
2017-10-03 22:48:10 +0100

    incomplete..

---
+++ Code/27GitAutomated.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python27
+
+import subprocess
+import os
+
+def update_file():
+    filepath =  "/Python/Code/numbers.txt"
+    command = "/usr/bin/git pull /Python/Code"
+    process = subprocess.Popen(command, stdout = subprocess.PIPE ,stderr = subprocess.PIPE, shell=True)
+    print process
+
+def main():
+    update_file()
+
+if __name__ == "__main__":
+    main()
