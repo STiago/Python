a9a3240e6fa28388e9f2bdeeeebf4e32af3b9d49
STiago <victoriasantiago@correo.ugr.com>
2018-05-12 07:36:53 -0800

    Task 12/05/2018

--- Code/airports.py
+++ Code/airports.py
@@ -2,49 +2,69 @@
 # Write outputs to Standard Output.
 # Please, do not use fileinput module to read Standard Input.
 
-
 import sys
 
+adj = {} #adjacency list
+
+dist = {}
+
+def get_distance(road):
+    pass
+    #in my home dir
+
+def find_routes(node, destination, routes, current_road, visited):
+
+    visited.add(node)
+    current_road.append(node)
+
+    print "Step ", node, destination, "Routes:", routes, "Current_road ", current_road, visited, "\n"
+
+    #reached destination
+    if node == destination:
+        routes.append(list(current_road))
+    if node not in adj:
+        return
+
+    if node in adj:
+        for i in adj[node]:
+            if i not in visited:
+                #add
+                #recurse
+                find_routes(i, destination, routes, current_road, visited)
+
+                #take out
+                del current_road[-1]
+                visited.remove(i)
+
+
+
 def findRoutes(source, destination):
-    depart = []
-    arrive = []
-    
-    for i in range(0,n):
-        if segments[i] == source:
-            depart.append(segment[i])
-        else:
-            print "[ERROR] - Source empty"
-            
-    #depart = depart.split()
-    dep = depart[1]
-    
-    for i in depart:
-        if depart[1] == destination:
-            arrive.append(depart[i])
-        elif dep[i] == depart[0]:
-            intermedite = dep[i]
-            findRoutes(intermedite,destination)
-    return arrive
+    pass
+    #in my home dir
 
-    ######################################
-    ######################################
-    ###        writing                   #
-    #######################################
-    ######################################
+n = int(raw_input())
+origin = raw_input()
+destination = raw_input()
 
-if __name__ == '__main__':
-    n = int(raw_input())
-    origin = raw_input()
-    destination = raw_input()
-    segments = []
-    solution = []
-    
-    for i in xrange(n):
-        segments.append(raw_input())
-    
-    solution = findRoutes(origin, destination)
-    
-    #for i in range solution:
-    #    print solution[i]
-    
-    print solution
+print n, origin, destination
+segments = []
+
+for i in xrange(n):
+    segments.append(raw_input())
+    a, b, dis = segments[-1].split(" ")
+    print a,b, dis
+    if a in adj:
+        adj[a].append(b)
+    else:
+        adj[a] = [b]
+
+    dist[ (a,b) ] = dis
+
+print adj, dist
+
+routes = []
+
+find_routes(origin, destination, routes, [], set())
+
+print routes
+
